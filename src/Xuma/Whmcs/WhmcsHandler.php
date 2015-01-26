317a11727cef83140b9210abe074354599c5da0c
Hakan ERSU <hakanersu@gmail.com>
2015-01-26 11:53:25 +0200

    Config

--- src/Xuma/Whmcs/WhmcsHandler.php
+++ src/Xuma/Whmcs/WhmcsHandler.php
@@ -3,5 +3,16 @@
 use Exception;
 use Httpful\Http;
 class WhmcsHandler{
+    public function test()
+    {
+        $uri = 'https://github.com/api/v2/xml/user/show/nategood';
 
+        $response = Request::get($uri)  // Will parse based on Content-Type
+        ->expectsXml()              // from the response, but can specify
+        ->send();                   // how to parse via expectsXml too.
+
+        echo "{$request->body->name} joined GitHub on " .
+            date('M jS', strtotime($request->body->{'created-at'})) ."\n";
+    }
+    
 }

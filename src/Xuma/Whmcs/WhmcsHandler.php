3b36ba926d3844a1b91ae05aed8f55883cfad639
Hakan ERSU <hakanersu@gmail.com>
2015-01-29 12:08:51 +0200

    Support reply and new ticket added

--- src/Xuma/Whmcs/WhmcsHandler.php
+++ src/Xuma/Whmcs/WhmcsHandler.php
@@ -109,8 +109,27 @@ class WhmcsHandler extends WhmcsConnector{
         return $response;
     }
 
-    public function postTicketReply()
+    /**
+     * Post a ticket reply
+     * @param $params
+     * @return bool
+     */
+    public function postTicketReply($params)
     {
+        $response= $this->getJson('addticketreply',$params);
 
+        return $response->result=="success" ? true :false;
+    }
+
+    /**
+     * Create a new ticket.
+     * @param $params
+     * @return bool
+     */
+    public function postNewTicket($params)
+    {
+        $response= $this->getJson('openticket',$params);
+
+        return $response->result=="success" ? true :false;
     }
 }

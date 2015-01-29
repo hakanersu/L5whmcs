48d630892c452626874b6c6c4a0680833f2f9a35
Hakan ERSU <hakanersu@gmail.com>
2015-01-29 15:48:05 +0200

    Custom invoice functions

--- src/Xuma/Whmcs/WhmcsHandler.php
+++ src/Xuma/Whmcs/WhmcsHandler.php
@@ -132,4 +132,30 @@ class WhmcsHandler extends WhmcsConnector{
 
         return $response->result=="success" ? true :false;
     }
+    
+    /**
+     * Get invoices.
+     * @param $params
+     * @return bool
+     */
+    public function getInvoices($params)
+    {
+        $response = $this->getJson('getinvoices',$params);
+
+        return ($response->numreturned>0) ? $response->invoices['invoice'] :false;
+    }
+
+    /**
+     * This is a custom api function that
+     * i create for getting invoices with items.
+     *
+     * @param $params
+     * @return bool
+     */
+    public function getCustomInvoices($params)
+    {
+        $response = $this->getJson('getinvoiceswithitems',$params);
+
+        return ($response->numreturned>0) ? $response->invoices :false;
+    }
 }
